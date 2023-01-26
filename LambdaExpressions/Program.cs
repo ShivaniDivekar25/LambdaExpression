63557f6499bfe3a12e39bb137809387d6443843f
ShivaniDivekar25 <shivanidivekar2@gmail.com>
2023-01-27 01:54:47 +0530

    Added Welcome To Lambda Expression

---
+++ LambdaExpressions/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace LambdaExpressions
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Lambda Expressions!");
+        }
+    }
+}
